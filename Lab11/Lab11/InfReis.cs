054c9b2e004ccba7d47880770ebb5fdbdfb9c3ba
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:40:29 +0200

    Create InfReis.cs

---
+++ Lab11/Lab11/InfReis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class InfReis
+    {
+        public string Reis { set; get; }
+        public int Ek_m { set; get; }
+        public int Biz_m { set; get; }
+        public InfReis(string reis,int ek_m,int biz_m)
+        {
+            Reis = reis;
+            Ek_m = ek_m;
+            Biz_m = biz_m;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Рейс:{Reis},Места эконома:{Ek_m},Места бизнес-класса:{Biz_m}");
+        }
+    }
+}
