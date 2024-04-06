0ed8e9277e2efee0a661e0992c814ab86881ad78
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:40:25 +0200

    Create Ekipaj.cs

---
+++ Lab11/Lab11/Ekipaj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Ekipaj
+    {
+        public int Kol_vo_ek { set; get; }
+        public string Av1 { set; get; }
+        public string Av2 { set; get; }
+        public Ekipaj(int kol_vo_ek,string av1,string av2)
+        {
+            Kol_vo_ek = kol_vo_ek;
+            Av1 = av1;
+            Av2 = av2;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Кол-во экипажа:{Kol_vo_ek},Первый пилот:{Av1},Второй пилот:{Av2}");
+        }
+    }
+}
