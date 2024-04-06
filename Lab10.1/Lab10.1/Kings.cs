3d96142ba1a7e12e8465f92d24a7250db2c973d4
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:39:15 +0200

    Create Kings.cs

---
+++ Lab10.1/Lab10.1/Kings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10._1
+{
+    class Kings:Country
+    {
+        public string Rej { set; get; }
+        public Kings(string name, int nasel, int s, string rej) : base(name, nasel, s)
+        {
+            Rej = rej;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Имя:{Name},Население:{Nasel},Площадь:{S},Режим:{Rej}");
+        }
+        public override void Sort()
+        {
+            if (Rej == "Демократия")
+            {
+                Print();
+            }
+        }
+
+    }
+}
