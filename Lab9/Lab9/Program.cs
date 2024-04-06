ef31e1d053eccd20d17d2e876e3e6d6ed1598ca0
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:43:40 +0200

    Create Program.cs

---
+++ Lab9/Lab9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("1-Добавление конуса");
+                Console.WriteLine("2-Добавление эллипса");
+                Console.WriteLine("3-Выход из добавления элементов");
+                switch(Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        pat.Conus();
+                        break;
+                    case '2':
+                        pat.Ellips();
+                        break;
+                    case '3':flag = false;
+                        break;
+                    default:Console.WriteLine("Ошибка ввода,попробуйте еще раз");
+                        break;
+                }
+            }
+            pat.Vivod();
+            pat.Viv1();
+            Console.ReadKey();
+        }
+    }
+}
