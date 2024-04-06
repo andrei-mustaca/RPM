21f2b0cd89d76340a1750edfc0f920b9a23a9e15
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:39:46 +0200

    Create Eat.cs

---
+++ Lab10/Lab6/Eat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Eat:Goods
+    {
+        public DateTime Date1 { set; get; }
+        public DateTime Date2 { set; get; }
+        public Eat(string name,int cost,DateTime date1,DateTime date2):base(name,cost)
+        {
+            Date1 = date1;
+            Date2 = date2;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Имя:{Name},Стоимость:{Cost},Дата производства:{Date1.ToShortDateString()},Дата истекающего срока годности:{Date2.ToShortDateString()}");
+        }
+        public override void Sort()
+        {
+            if(Date2<DateTime.Now)
+            {
+                Print();
+            }
+        }
+    }
+}
