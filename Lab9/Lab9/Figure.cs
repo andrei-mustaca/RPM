d69e645afc5f0fee4bdae624366ec71ed3ad7888
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:43:29 +0200

    Create Figure.cs

---
+++ Lab9/Lab9/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    abstract class Figure
+    {
+        public int R { set; get; }
+        public Figure(int r)
+        {
+            R = r;
+        }
+        public abstract void Print();
+        public abstract double S();
+        public abstract double V();
+    }
+}
