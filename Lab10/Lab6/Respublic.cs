ae5283e4f18bdb21cfa9b09439e400cc4e228052
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:40:07 +0200

    Create Respublic.cs

---
+++ Lab10/Lab6/Respublic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Respublic:Country
+    {
+        public string Rej { set; get; }
+        public Respublic(string name, int nasel, int s, string rej) : base(name, nasel, s)
+        {
+            Rej = rej;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Имя:{Name},Население:{Nasel},Площадь:{S},Режим:{Rej}");
+        }
+        public override void Sort()
+        {
+            if (Rej == "Демократия")
+            {
+                Print();
+            }
+        }
+    }
+}
