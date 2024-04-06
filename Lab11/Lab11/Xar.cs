0fc3d367564f59917d76d6f9657059f24cc646da
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:40:44 +0200

    Create Xar.cs

---
+++ Lab11/Lab11/Xar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Xar
+    {
+        public int Mass { set; get; }
+        public int V { set; get; }
+        public Xar(int mass,int v)
+        {
+            Mass = mass;
+            V = v;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Масса:{Mass},Скорость:{V}");
+        }
+    }
+}
