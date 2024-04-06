c8dad791560b21f82c6f128b9c9e92402da507b8
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:40:36 +0200

    Create Program.cs

---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Air> list = new List<Air>();
+                bool flag = true;
+                while(flag)
+                {
+                    Console.WriteLine("1-Добавление");
+                    Console.WriteLine("2-Выход");
+                    switch(Console.ReadKey(true).KeyChar)
+                    {
+                        case '1':list.Add(Air.Input());
+                            break;
+                        case '2':flag = false;
+                            break;
+                        default:Console.WriteLine("Ошибка ввода");
+                            break;
+                    }
+                }
+                bool flag1 = true;
+                while (flag1)
+                {
+                    Console.WriteLine($"1-Вывод\n2-Изменение\n3-Выход из программы");
+                    switch (Console.ReadKey(true).KeyChar)
+                    {
+                        case '1':
+                            foreach (var elem in list)
+                            {
+                                elem.Print();
+                            }
+                            break;
+                        case '2':
+                            Console.Write("Введите элемент коллекции:");
+                            int n = Convert.ToInt32(Console.ReadLine());
+                            list[n] = Air.Input();
+                            break;
+                        case '3': return;
+                        default:
+                            Console.WriteLine("Ошибка ввода");
+                            break;
+                    }
+                }
+               
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.ReadKey();
+        }
+    }
+}
