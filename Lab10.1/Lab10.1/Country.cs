39f8005592a2a6a89d2b52829bca2ca95f9efde8
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:39:04 +0200

    Create Country.cs

---
+++ Lab10.1/Lab10.1/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10._1
+{
+    abstract class Country
+    {
+        public string Name { set; get;}
+        public int Nasel { set; get; }
+        public int S { set; get; }
+        public Country(string name,int nasel,int s)
+        {
+            Name = name;
+            Nasel = nasel;
+            S = s;
+        }
+        public abstract void Print();
+        public abstract void Sort();
+    }
+}
