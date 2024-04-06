962e4dd84831621f38e40b5be5f0610b44babbf9
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:39:11 +0200

    Create Goods.cs

---
+++ Lab10.1/Lab10.1/Goods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab10._1
+{
+    abstract class Goods
+    {
+        public string Name { set; get; }
+        public int Cost { set; get; }
+        public Goods() { }
+        public Goods(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+        public abstract void Print();
+        public abstract void Sort();
+
+    }
+}
