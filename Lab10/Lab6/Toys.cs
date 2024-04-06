f0172ff199f5e262d94c37b09dc8496e1d227137
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-06 08:40:10 +0200

    Create Toys.cs

---
+++ Lab10/Lab6/Toys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Toys:Goods
+    {
+        public Toys(string name,int cost):base(name,cost)
+        {
+
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Имя:{Name},Cost:{Cost}");
+        }
+        public override void Sort()
+        {
+            if(Cost>5000)
+            {
+                Print();
+            }
+        }
+    }
+}
